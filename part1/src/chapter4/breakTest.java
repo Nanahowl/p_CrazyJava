55632abc2792822c57ee5602bbee5319519af33f
Nina Zhao <nina_zhao77@163.com>
2019-03-06 11:50:21 +0800

    inital branch

---
+++ part1/src/chapter4/breakTest.java
@@ -0,0 +1,12 @@
+package chapter4;
+
+public class breakTest {
+    public static void main(String[] args){
+        for (int i=1;i<10;i++){
+            System.out.println("i is: " +i);
+        if(i==3){
+            break;
+        }
+        }
+    }
+}
